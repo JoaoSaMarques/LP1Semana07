2e95feb31e489a5aac5fa205a682eb37ea49c3f1
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-26 16:47:57 +0100

    Added green

--- FillSpheres/Color.cs
+++ FillSpheres/Color.cs
@@ -4,18 +4,20 @@ namespace FillSpheres
 {
     public class Color
     {
-        //Colors
+        //Colors RGB
         public int Red { get; set }
+        public int Green { get; set }
 
 
         //Define color
-        public Color(int red)
+        public Color(int red, int green)
         {
             Red = red;
+            Green = green;
         }
 
         
-        public Color(int red) : this(red)
+        public Color(int red, int green) : this(red, green)
         {
 
         }
