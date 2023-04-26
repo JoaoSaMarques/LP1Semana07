0f529253065f2107eb4819a85b546716286642c3
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-26 17:00:59 +0100

    Sphere definition

--- FillSpheres/Sphere.cs
+++ FillSpheres/Sphere.cs
@@ -8,7 +8,16 @@ namespace FillSpheres
         private double radius;
         private int numOfTimesThrown;
 
+        //Define the sphere
         public Sphere(Color color, double radius)
+        {
+            //Color
+            this.color = color;
+            //Total radius
+            this.radius = radius;
+            //Times it has been thrown
+            this.timesThrown = 0;
+        }
 
 
 
