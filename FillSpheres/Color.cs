e89a49c69230892f974a765f361075b8a9315d68
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-26 16:49:18 +0100

    Added alpha

--- FillSpheres/Color.cs
+++ FillSpheres/Color.cs
@@ -8,18 +8,20 @@ namespace FillSpheres
         public int Red { get; set }
         public int Green { get; set }
         public int Blue { get; set }
+        public int Alpha { get; set }
 
 
         //Define color
-        public Color(int red, int green, int blue)
+        public Color(int red, int green, int blue, int alpha)
         {
             Red = red;
             Green = green;
             Blue = blue;
+            Alpha = alpha;
         }
 
         
-        public Color(int red, int green, int blue) : this(red, green, blue)
+        public Color(int red, int green, int blue) : this(red, green, blue, 255)
         {
 
         }
