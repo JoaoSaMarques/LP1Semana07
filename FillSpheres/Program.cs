858335d0de34d9982a727a82d01697485478493f
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-26 17:18:04 +0100

    throw spheres

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -10,6 +10,15 @@ namespace FillSpheres
             Sphere sphere1 = new Sphere(new Color(255, 0, 0), 5);
             Sphere sphere2 = new Sphere(new Color(0, 255, 0), 10);
             Sphere sphere3 = new Sphere(new Color(0, 0, 255), 15);
+
+            //Throw the spheres around
+            //Throwing 2 of each sphere
+            sphere1.Throw();
+            sphere1.Throw();
+            sphere2.Throw();
+            sphere2.Throw();
+            sphere3.Throw();
+            sphere3.Throw();
         }
     }
 }
