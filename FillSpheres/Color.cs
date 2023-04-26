d9d8ded19217653cc7d23d1828ef8b11baba9899
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-26 16:47:02 +0100

    Added red

---
+++ FillSpheres/Color.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FillSpheres
+{
+    public class Color
+    {
+        //Colors
+        public int Red { get; set }
+
+
+        //Define color
+        public Color(int red)
+        {
+            Red = red;
+        }
+
+        
+        public Color(int red) : this(red)
+        {
+
+        }
+
+    }
+}
