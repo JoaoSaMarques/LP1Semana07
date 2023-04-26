bd7243f197bb46edf32caace87390661a7696912
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-26 17:17:17 +0100

    create spheres

--- FillSpheres/Program.cs
+++ FillSpheres/Program.cs
@@ -6,7 +6,10 @@ namespace FillSpheres
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Create some spheres
+            Sphere sphere1 = new Sphere(new Color(255, 0, 0), 5);
+            Sphere sphere2 = new Sphere(new Color(0, 255, 0), 10);
+            Sphere sphere3 = new Sphere(new Color(0, 0, 255), 15);
         }
     }
 }
