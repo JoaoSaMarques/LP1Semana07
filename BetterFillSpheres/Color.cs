5e0ff51c2556ce8aaaa4063dd1183d694045925d
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-27 16:35:07 +0100

    Reverted

--- BetterFillSpheres/Color.cs
+++ BetterFillSpheres/Color.cs
@@ -1,51 +1,58 @@
-﻿public class Color
+﻿using System;
+
+namespace FillSpheres
 {
-    //RGB + Alpha
-    private int _red;
-    private int _green;
-    private int _blue;
-    private int _alpha;
+    public class Color
+    {
+        //Colors RGB
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+        public int Alpha { get; set; }
 
-    //Red
-    public int Red 
-    {
-        get { return _red; }
-        set { _red = value; }
-    }
+        //Define color
+        public Color(int red, int green, int blue, int alpha)
+        {
+            //Red is red
+            Red = red;
+            //Green is green
+            Green = green;
+            //Blue is blue
+            Blue = blue;
+            //Alpha is alpha
+            Alpha = alpha;
+        }
 
-    //Green
-    public int Green 
-    {
-        get { return _green; }
-        set { _green = value; }
-    }
+        //Color
+        public Color(int red, int green, int blue) : this(red, green, blue, 255)
+        {
+        }
 
-    //Blue
-    public int Blue 
-    {
-        get { return _blue; }
-        set { _blue = value; }
-    }
-    
-    //Alpha
-    public int Alpha 
-    {
-        get { return _alpha; }
-        set { _alpha = value; }
-    }
+        //Grey
+        public int GetGrey()
+        {
+            return (Red + Green + Blue) / 3;
+        }
 
-    //Color
-    public Color(int red, int green, int blue, int alpha = 255)
-    {
-        Red = red;
-        Green = green;
-        Blue = blue;
-        Alpha = alpha;
-    }
-
-    //Grey
-    public int GetGrey()
-    {
-        return (Red + Green + Blue) / 3;
+        //Get the name of the color
+        public string GetName()
+        {
+            if (Red == 255 && Green == 0 && Blue == 0) // Red
+            {
+                return "Red";
+            }
+            else if (Red == 0 && Green == 255 && Blue == 0) // Green
+            {
+                return "Green";
+            }
+            else if (Red == 0 && Green == 0 && Blue == 255) // Blue
+            {
+                return "Blue";
+            }
+            else //Mixed color
+            {
+                return "Mixed";
+            }
+        }
     }
 }
