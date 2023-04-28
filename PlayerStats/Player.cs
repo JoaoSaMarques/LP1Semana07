cc0b7d31a26b07b838dfb9e3ce23ee8533432cb9
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-28 10:52:58 +0100

    string name

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -4,9 +4,26 @@ namespace PlayerStats
 {
     public class Player
     {
+        //stats
+        //HighScore
         public float highScore { get; set; }
+        //Amount of games played
         public int playedGames { get; set; }
+        //Games won
         public int wonGames { get; set; }
+        //String name
+        public readonly string name;
+
+        //Player stats
+        public Player(float HighScore)
+        {
+            highScore = HighScore
+        }
+
+        
+
+
+
 
     }
 }
