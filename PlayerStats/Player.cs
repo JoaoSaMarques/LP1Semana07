3dc3f6335e68df3c2e1355df432fd1d7f40a41d3
JoaoSaMarques <joao.sa.marques@hotmail.com>
2023-04-28 10:39:21 +0100

    Highscore, playedgames, wongames

--- PlayerStats/Player.cs
+++ PlayerStats/Player.cs
@@ -4,6 +4,9 @@ namespace PlayerStats
 {
     public class Player
     {
-        
+        public float highScore { get; set; }
+        public int playedGames { get; set; }
+        public int wonGames { get; set; }
+
     }
 }
