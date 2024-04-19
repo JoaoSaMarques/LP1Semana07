30b16aac7c3ccbb549f65263f3b0809fbf69f9be
JoaoSaMarques <joao.sa.marques@hotmail.com>
2024-04-19 13:25:06 +0100

    created SpyUnit with secretinfo

--- GameUnits/SpyUnit.cs
+++ GameUnits/SpyUnit.cs
@@ -2,24 +2,36 @@ using System;
 
 namespace GameUnits
 {
-    public class SpyUnit: Unit
+    public class SpyUnit : XPUnit
     {
-        public SpyUnit() : base(2, 5)
+        public SpyUnit() : base(8, 2)
         {
-            Movement = 1;
-            Health = 2;
+            Cost = 12.5;
         }
 
-        public override float Health
+        public double Cost { get; protected set; }
+
+        public override string ToString()
         {
-            get { return base.Health; }
-            set { base.Health = value; }
+            return base.ToString() + " Cost: " + Cost;
         }
 
-        public override float Cost
+        public void GetSecretInfo(Unit unit)
         {
-            get { return base.Cost; }
-            set { base.Cost = value; }
+            if (unit is SpyUnit)
+            {
+                XP += 3;
+            }
+            
+            if (unit is MilitaryUnit)
+            {
+                XP += 2;
+            }
+
+            else
+            {
+                XP += 1;
+            }
         }
     }
 }
