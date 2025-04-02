9b4fc8eed235df4d32778f9f22d744b7f8842731
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-04-02 11:24:13 +0100

    update Color

--- BetterColorSpheres/Color.cs
+++ BetterColorSpheres/Color.cs
@@ -1,37 +1,32 @@
-﻿using System;
+﻿// Color.cs
+using System;
 
-namespace ColorSpheres
+namespace BetterColorSpheres
 {
     public class Color
     {
-        private readonly int red;
-        private readonly int green;
-        private readonly int blue;        
-        private readonly int alpha;
+        public int Red { get; }
+        public int Green { get; }
+        public int Blue { get; }
+        public int Alpha { get; }
 
         // Constructor with all parameters
         public Color(int red, int green, int blue, int alpha)
         {
-            this.red = Clamp(red);
-            this.green = Clamp(green);
-            this.blue = Clamp(blue);
-            this.alpha = Clamp(alpha);
+            Red = Clamp(red);
+            Green = Clamp(green);
+            Blue = Clamp(blue);
+            Alpha = Clamp(alpha);
         }
 
         // Constructor with RGB, alpha defaults to 255
         public Color(int red, int green, int blue) 
-        : this(red, green, blue, 255) { }
-
-        // Getters
-        public int Red => red;
-        public int Green => green;
-        public int Blue => blue;
-        public int Alpha => alpha;
+            : this(red, green, blue, 255) { }
 
         // Method to get grey value
         public int GetGrey()
         {
-            return (red + green + blue) / 3;
+            return (Red + Green + Blue) / 3;
         }
 
         // Helper method to clamp values between 0 and 255
