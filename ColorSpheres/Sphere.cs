d428327d40526e7311e6d8198ce567ed01f6ed96
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-04-02 11:04:34 +0100

    add parameters

--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -2,11 +2,10 @@
 
 namespace ColorSpheres
 {
-    public class Program
+    public class Sphere
     {
-        private static void Main(string[] args)
-        {
-            Console.WriteLine("Hello LP1!");
-        }
+        private Color color;
+        private double radius;
+        private int timesThrown;
     }
 }
