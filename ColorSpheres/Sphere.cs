c4423afcaf796311db6ebbdcdd6c5a0372d3243c
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-04-02 11:04:55 +0100

    create constructor

--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -7,5 +7,13 @@ namespace ColorSpheres
         private Color color;
         private double radius;
         private int timesThrown;
+
+        // Constructor
+        public Sphere(Color color, double radius)
+        {
+            this.color = color;
+            this.radius = radius;
+            this.timesThrown = 0;
+        }
     }
 }
