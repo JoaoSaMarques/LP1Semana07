ffeddb383d3da7d9b3553777fdfd94cdab7de41a
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-04-02 11:01:48 +0100

    add constructor for color

--- ColorSpheres/Color.cs
+++ ColorSpheres/Color.cs
@@ -8,5 +8,16 @@ namespace ColorSpheres
         private int green;
         private int blue;
         private int alpha;
+
+        // Constructor with all parameters
+        public Color(int red, int green, int blue, int alpha)
+        {
+            this.red = Clamp(red);
+            this.green = Clamp(green);
+            this.blue = Clamp(blue);
+            this.alpha = Clamp(alpha);
+        }
+
+
     }
 }
