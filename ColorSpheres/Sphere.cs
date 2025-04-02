d10074f19c229ab9bd86c2e9e4e2a8691d0ab983
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-04-02 11:13:18 +0100

    change sphere to readonly

--- ColorSpheres/Sphere.cs
+++ ColorSpheres/Sphere.cs
@@ -4,9 +4,9 @@ namespace ColorSpheres
 {
     public class Sphere
     {
-        private Color color;
+        private readonly Color color;
         private double radius;
-        private int timesThrown;
+        private readonly int timesThrown;
 
         // Constructor
         public Sphere(Color color, double radius)
