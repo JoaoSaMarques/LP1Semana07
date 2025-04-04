acfe6432350db6f474012546403c1a1575ecf81d
JoaoSaMarques <joao.sa.marques@hotmail.com>
2025-04-04 14:19:21 +0100

    Add proprities

--- MyRPG/Player.cs
+++ MyRPG/Player.cs
@@ -2,24 +2,45 @@
 {
     public class Player
     {
-        readonly string name;
+        private string name;
         private int xp;
         private float health;
         
         readonly int level;
 
 
-        public string Name { get; }
-
-        // Constructor
-        public Player(string name, int xp, float health, int level)
-        {
-            Name = name;
-            this.xp = xp;
-            this.health = health;
-            this.level = level;
+        readonly string Name { get { return name; } }
+        public int Level { get; }
+        public float Health 
+        { 
+            get 
+            { 
+                return health; 
+            } 
+            set
+            {
+                health = value;
+                if (health <= 0)
+                {
+                    health = 0;
+                }
+            }
         }
 
-        public GetHealth()
+        public int XP { get { return xp; } }
+
+        // Constructor
+        public Player(string name)
+        {
+            Name = name;
+            xp = 0;
+            health = MaxHealth;
+            level = 1 + xp / 1000;
+        }
+
+        public int GetLevel()
+        {
+            return level;
+        }
     }
 }
